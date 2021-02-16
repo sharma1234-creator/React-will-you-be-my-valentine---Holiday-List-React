5115947da8e5fb42f93bbfbff0fd073c2a02ff16
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-02-16 23:33:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -41,22 +41,22 @@ class App extends Component {
     return (
       <div id="main">
         <ol>
-             <li>{this.cityList.name[0]}, {this.cityList.country[0]}</li>
-             <li>{this.cityList.name[3]}, {this.cityList.country[3]}</li>
-             <li>{this.cityList.name[5]}, {this.cityList.country[5]}</li>
-             <li>{this.cityList.name[9]}, {this.cityList.country[9]}</li>
-             <li>{this.cityList.name[10]}, {this.cityList.country[10]}</li>
-             <li>{this.cityList.name[11]}, {this.cityList.country[11]}</li>
-             <li>{this.cityList.name[12]}, {this.cityList.country[12]}</li>
-             <li>{this.cityList.name[13]}, {this.cityList.country[13]}</li>
-             <li>{this.cityList.name[14]}, {this.cityList.country[14]}</li>
-             <li>{this.cityList.name[18]}, {this.cityList.country[18]}</li>
-             <li>{this.cityList.name[19]}, {this.cityList.country[19]}</li>
-             <li>{this.cityList.name[23]}, {this.cityList.country[23]}</li>
-             <li>{this.cityList.name[24]}, {this.cityList.country[24]}</li>
-             <li>{this.cityList.name[25]}, {this.cityList.country[25]}</li>
-             <li>{this.cityList.name[26]}, {this.cityList.country[26]}</li>
-              <li>{this.cityList.name[27]}, {this.cityList.country[27]}</li>
+             <li>{this.cityList.name[0]}</li>
+             <li>{this.cityList.name[3]}</li>
+             <li>{this.cityList.name[5]}</li>
+             <li>{this.cityList.name[9]}</li>
+             <li>{this.cityList.name[10]}</li>
+             <li>{this.cityList.name[11]}</li>
+             <li>{this.cityList.name[12]}</li>
+             <li>{this.cityList.name[13]}</li>
+             <li>{this.cityList.name[14]}</li>
+             <li>{this.cityList.name[18]}</li>
+             <li>{this.cityList.name[19]}</li>
+             <li>{this.cityList.name[23]}</li>
+             <li>{this.cityList.name[24]}</li>
+             <li>{this.cityList.name[25]}</li>
+             <li>{this.cityList.name[26]}</li>
+              <li>{this.cityList.name[27]}</li>
              
       </ol>
       </div>
