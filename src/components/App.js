cb22d009813d16c7db00942b4ecfa562d511e8d7
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-02-24 12:13:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -43,10 +43,10 @@ class App extends Component {
         <ol>
              <li key="location1">{this.cityList[0].name}</li>
              <li key="location2">{this.cityList[3].name}</li>
-             <li key="location3">{this.cityList[5].name}</li>
-             <li key="location4">{this.cityList[9].name}</li>
-             <li key="location5">{this.cityList[10].name}</li>
-             <li key="location6">{this.cityList[12].name}</li>
+            // <li key="location3">{this.cityList[5].name}</li>
+            // <li key="location4">{this.cityList[9].name}</li>
+            // <li key="location5">{this.cityList[10].name}</li>
+           //  <li key="location6">{this.cityList[12].name}</li>
             // <li key="location7">{this.cityList[12].name}</li>
              //<li key="location8">{this.cityList[13].name}</li>
              //<li key="location9">{this.cityList[14].name}</li>
