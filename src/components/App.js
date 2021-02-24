f05c001daa6383328aa011323f4a81f740ac9398
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-02-24 12:40:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-  constructor(props) {
+    constructor(props) {
     super(props)
 
     this.cityList = [{ name: 'Goa', country: 'India' },
@@ -11,31 +10,42 @@ class App extends Component {
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' }
-    ]
+    { name: 'Lonavala', country: 'India' },
+    ];
+    this.list2=this.FilterElement();
+    this.liList=this.wrap();
+  }
+
+  wrap(){
+      console.log(this.list2);
+    let c=0;
+    const wrapcity=this.list2.map((city)=>{
+        let keyloc='location';
+        return(
+            <li key={keyloc+(c++)}>{city}</li>
+        );
+    });
+    return(
+        <ol>{wrapcity}</ol>
+    )
+  }
+ 
+  FilterElement() {
+    let indCity=[];
+    this.cityList.forEach((city)=>{
+        if(city.country=='India' && !indCity.includes(city.name)){
+            indCity.push(city.name);
+        }
+            
+    });
+    
+    return indCity;
   }
 
   render() {
     return (
       <div id="main">
-        <ol>
-             <li key="location1">{this.cityList[0].name}</li>
-             <li key="location2">{this.cityList[3].name}</li>
-             <li key="location3">{this.cityList[5].name}</li>
-            // <li key="location4">{this.cityList[9].name}</li>
-            // <li key="location5">{this.cityList[10].name}</li>
-           //  <li key="location6">{this.cityList[12].name}</li>
-            // <li key="location7">{this.cityList[12].name}</li>
-             //<li key="location8">{this.cityList[13].name}</li>
-             //<li key="location9">{this.cityList[14].name}</li>
-             //<li key="location10">{this.cityList[18].name}</li>
-             //<li key="location11">{this.cityList[19].name}</li>
-             //<li key="location12">{this.cityList[23].name}</li>
-             //<li key="location13">{this.cityList[24].name}</li>
-             //<li key="location14">{this.cityList[25].name}</li>
-             //<li key="location15">{this.cityList[26].name}</li>
-              //<li key="location16">{this.cityList[27].name}</li>
-          </ol>
+        {this.liList}
       </div>
     )
   }
